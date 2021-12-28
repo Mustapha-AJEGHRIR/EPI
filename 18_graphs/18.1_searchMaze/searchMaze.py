78f01ae1bf6847afe9395bad791946a598995d2f
Mustapha-AJEGHRIR <m5u9s00@gmail.com>
2021-12-28 13:01:48 +0100

    Building graph done, adding more tests

--- 18_graphs/18.1_searchMaze/searchMaze.py
+++ 18_graphs/18.1_searchMaze/searchMaze.py
@@ -11,6 +11,14 @@
 import numpy as np
 import os
 
+# Some constents
+UP = np.array([-1, 0])
+DOWN = np.array([1, 0])
+LEFT = np.array([0, -1])
+RIGHT = np.array([0, 1])
+
+
+
 def char_mapper(x : str) -> bool:
     """
     Returns if we can walk on x
@@ -22,7 +30,7 @@ def char_mapper(x : str) -> bool:
     else:
         raise ValueError("Unknown value in the input : " + x)
 
-def read_map_from_input(path = os.path.join(os.path.dirname(__file__), "maze1.txt") ) -> np.ndarray:
+def read_map_from_input(path = os.path.join(os.path.dirname(__file__), "maze1.txt") ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     reads the input map of the maze, like "maze1" in the folder
     Returns:
@@ -30,17 +38,75 @@ def read_map_from_input(path = os.path.join(os.path.dirname(__file__), "maze1.tx
     """
     with open(path, "r") as f:
         n, m = [int(x) for x in f.readline().split()]
-        s = tuple(map(int, f.readline().split()))
-        e = tuple(map(int, f.readline().split()))
+        s = np.array(tuple(map(int, f.readline().split())))
+        e = np.array(tuple(map(int, f.readline().split())))
         lignes = []
         for _ in range(n):
             raw_ligne = list(f.readline().strip())
             lignes.append(list(map(char_mapper, raw_ligne)))
     return np.array(lignes), s, e        
 
+def get_neighbors_raw_map(pos : np.ndarray, maze_map : np.ndarray) -> list[tuple[np.ndarray, np.ndarray,np.ndarray]]: #new_pos, parent,  direction
+    """
+    Returns the neighbors of pos in the maze_map
+    """
+    neighbors = []
+    for d in [UP, DOWN, LEFT, RIGHT]:
+        new_pos = pos + d
+        if new_pos[0] >= 0 and new_pos[1] >= 0 and new_pos[0] < maze_map.shape[0] and new_pos[1] < maze_map.shape[1]:
+            if maze_map[new_pos[0], new_pos[1]]:
+                neighbors.append((new_pos, pos, d))
+    return neighbors
+
+def make_node(pos: np.ndarray, maze_map: np.ndarray) -> bool:
+    """
+        Returns if this position is simple (straight line) or complex
+    """
+    neighbors = get_neighbors_raw_map(pos, maze_map)
+    if len(neighbors) != 2:
+        return True
+    if len(neighbors) == 2:
+        n1, n2 = neighbors
+        if np.all(n1[2] == -n2[2]):
+            return False
+        else:
+            return True
+        
+def build_graph(maze_map : np.ndarray, s : np.ndarray, e : np.ndarray) -> tuple[np.ndarray, set[tuple]]:
+    V = np.zeros(maze_map.shape, dtype = bool)
+    V[tuple(s)] = True
+    V[tuple(e)] = True
+    E = set()
+    n, m = maze_map.shape
+    for i in range (n):
+        for j in range(m):
+            if maze_map[i, j]:
+                pos = np.array([i, j])
+                if make_node(pos, maze_map):
+                    V[tuple(pos)] = True
+    for i in range(n):
+        for j in range(m):
+            if maze_map[i, j]:
+                pos = np.array([i, j])
+                if make_node(pos, maze_map):
+                    directions = map( lambda x: x[2], get_neighbors_raw_map(pos, maze_map))
+                    for d in directions:
+                        index = 1
+                        while not V[tuple(pos + index * d)]:
+                            index += 1
+                        E.add((tuple(pos), tuple(pos + index * d), index))
+                        E.add((tuple(pos + index * d), tuple(pos), index))
+    return V, E
+            
+    
+    
+
 
 if __name__ == "__main__":
     # s represents the start position, e represents the end position
     maze_map, s, e = read_map_from_input()
-    print(maze_map)
+    V, E = build_graph(maze_map, s, e)
+
+    
+    
     
--- 18_graphs/18.1_searchMaze/test_searchMaze.py
+++ 18_graphs/18.1_searchMaze/test_searchMaze.py
@@ -1,14 +1,15 @@
+from types import BuiltinFunctionType
 import unittest
 import os
 import numpy as np
-from searchMaze import read_map_from_input
+from searchMaze import read_map_from_input, build_graph
 
 class TestMapReading(unittest.TestCase):
     def test_read_from_input(self):
         path = os.path.join(os.path.dirname(__file__), "maze1.txt")
         map, s, e = read_map_from_input(path)
-        true_s = (7, 0) 
-        true_e = (0, 7)
+        true_s = np.array((7, 0)) 
+        true_e = np.array((0, 7))
         true_map = np.array([[False,  True,  True,  True, False, False,  True,  True],
                                 [False,  True, False,  True, False,  True,  True,  True],
                                 [False,  True, False,  True,  True,  True, False, False],
@@ -18,8 +19,10 @@ class TestMapReading(unittest.TestCase):
                                 [False,  True,  True,  True, False,  True,  True,  True],
                                 [ True,  True, False, False, False,  True,  True,  True]])
         #check e and s
-        self.assertEqual(s, true_s)
-        self.assertEqual(e, true_e)
+        self.assertIsInstance(s, np.ndarray)
+        self.assertIsInstance(e, np.ndarray)
+        self.assertTrue(np.all(s == true_s))
+        self.assertTrue(np.all(e == true_e))
         #type check 
         self.assertIsInstance(map, np.ndarray)
         #shape check
@@ -27,6 +30,51 @@ class TestMapReading(unittest.TestCase):
         #content check
         self.assertTrue(np.all(map == true_map))
 
+
+class TestGraphBuilding(unittest.TestCase):
+    def test_graph(self):
+        path = os.path.join(os.path.dirname(__file__), "maze1.txt")
+        map, s, e = read_map_from_input(path)
+        V, E = build_graph(map, s, e)
+        true_V = np.array([[False,  True, False,  True, False, False,  True,  True],
+                                [False, False, False, False, False,  True,  True,  True],
+                                [False, False, False,  True, False,  True, False, False],
+                                [False,  True,  True, False, False,  True,  True, False],
+                                [False,  True,  True,  True, False, False,  True,  True],
+                                [False, False, False,  True, False,  True,  True,  True],
+                                [False,  True, False,  True, False,  True,  True,  True],
+                                [ True,  True, False, False, False,  True,  True,  True]])
+        true_E = np.array({((6, 7), (7, 7), 1), ((4, 6), (4, 7), 1), ((6, 3), (6, 1), 2),
+                            ((5, 7), (4, 7), 1), ((0, 6), (1, 6), 1), ((1, 5), (2, 5), 1),
+                            ((6, 6), (5, 6), 1), ((6, 5), (7, 5), 1), ((6, 1), (6, 3), 2),
+                            ((7, 0), (7, 1), 1), ((1, 6), (1, 5), 1), ((4, 1), (3, 1), 1),
+                            ((1, 6), (1, 7), 1), ((4, 7), (4, 6), 1), ((5, 5), (5, 6), 1),
+                            ((0, 1), (3, 1), 3), ((5, 3), (5, 5), 2), ((6, 5), (6, 6), 1),
+                            ((2, 3), (2, 5), 2), ((0, 7), (1, 7), 1), ((4, 6), (5, 6), 1),
+                            ((4, 2), (3, 2), 1), ((1, 7), (1, 6), 1), ((5, 7), (5, 6), 1),
+                            ((3, 1), (3, 2), 1), ((2, 5), (2, 3), 2), ((7, 7), (6, 7), 1), 
+                            ((4, 3), (4, 2), 1), ((3, 1), (4, 1), 1), ((3, 5), (2, 5), 1),
+                            ((7, 5), (7, 6), 1), ((2, 3), (0, 3), 2), ((3, 1), (0, 1), 3),
+                            ((6, 7), (5, 7), 1), ((4, 1), (4, 2), 1), ((3, 2), (4, 2), 1),
+                            ((7, 1), (6, 1), 1), ((5, 6), (5, 5), 1), ((4, 2), (4, 3), 1),
+                            ((6, 6), (6, 5), 1), ((0, 6), (0, 7), 1), ((2, 5), (3, 5), 1),
+                            ((6, 1), (7, 1), 1), ((6, 3), (5, 3), 1), ((7, 1), (7, 0), 1),
+                            ((7, 6), (6, 6), 1), ((0, 3), (2, 3), 2), ((6, 6), (6, 7), 1),
+                            ((5, 7), (6, 7), 1), ((2, 5), (1, 5), 1), ((4, 2), (4, 1), 1),
+                            ((4, 7), (5, 7), 1), ((5, 3), (6, 3), 1), ((6, 7), (6, 6), 1),
+                            ((7, 7), (7, 6), 1), ((5, 5), (5, 3), 2), ((0, 1), (0, 3), 2),
+                            ((7, 6), (7, 7), 1), ((5, 6), (5, 7), 1), ((1, 5), (1, 6), 1),
+                            ((5, 6), (4, 6), 1), ((6, 5), (5, 5), 1), ((3, 2), (3, 1), 1),
+                            ((0, 3), (0, 1), 2), ((1, 6), (0, 6), 1), ((4, 6), (3, 6), 1),
+                            ((3, 5), (3, 6), 1), ((5, 5), (6, 5), 1), ((5, 3), (4, 3), 1),
+                            ((3, 6), (3, 5), 1), ((1, 7), (0, 7), 1), ((0, 7), (0, 6), 1),
+                            ((7, 6), (7, 5), 1), ((7, 5), (6, 5), 1), ((5, 6), (6, 6), 1),
+                            ((4, 3), (5, 3), 1), ((3, 6), (4, 6), 1), ((6, 6), (7, 6), 1)})
+        #check e and s
+        self.assertIsInstance(V, np.ndarray)
+        self.assertIsInstance(E, set)
+        self.assertTrue(np.all(V == true_V))
+        self.assertTrue(E == true_E)
 
 
 if __name__ == "__main__":
